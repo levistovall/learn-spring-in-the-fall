124fd25b656b34edbc1341eeb2c94342bced1d68
Levi Stovall <levi.stovall713@gmail.com>
2020-03-21 17:27:26 -0500

    trying a hello world web page

---
+++ src/com/levi/LeviController.java
@@ -0,0 +1,15 @@
+package com.levi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/levi")
+public class LeviController {
+   @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
+      model.addAttribute("message", "I don't know what I'm doing!");
+      return "hello";
+   }
+}
