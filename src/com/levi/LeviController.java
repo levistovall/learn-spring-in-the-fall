27b259a3cdcc80782d07696847bb3f0e610743de
Levi Stovall <levi.stovall713@gmail.com>
2020-04-11 18:05:59 -0500

    update json student mapping so that spring doesn't try to find a view whose name is the returned string

--- src/com/levi/LeviController.java
+++ src/com/levi/LeviController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.ModelMap;
 
 @Controller
@@ -30,7 +31,8 @@ public class LeviController
 		return "viewall";
 	}
 
-	@RequestMapping(value="/getstudents", method=RequestMethod.GET)
+	@RequestMapping(value="/getstudents", method=RequestMethod.GET, produces="text,plain")
+	@ResponseBody
 	public String getAllStudents(ModelMap model)
 	{
 		return template.listStudentsAsJsonString();
