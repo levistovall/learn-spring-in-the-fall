b9702183a7f21a0269805fe3402d4d121b0fb6aa
Levi Stovall <levi.stovall713@gmail.com>
2020-04-10 22:42:40 -0500

    use latest spring version

--- src/com/levi/StudentMapper.java
+++ src/com/levi/StudentMapper.java
@@ -2,9 +2,9 @@ package com.levi;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 
-public class StudentMapper implements ParameterizedRowMapper<Student>
+public class StudentMapper implements RowMapper<Student>
 {
 	private final static String ID_COLUMN_NAME_ = "id";
 	private final static String NAME_COLUMN_NAME_ = "name";
