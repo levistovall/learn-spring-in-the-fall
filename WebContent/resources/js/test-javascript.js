b8761d8feba343bbdb289cae3325b551d977ca79
Levi Stovall <levi.stovall713@gmail.com>
2020-04-12 21:27:45 -0500

    just playing around with the chart

--- WebContent/resources/js/test-javascript.js
+++ WebContent/resources/js/test-javascript.js
@@ -69,37 +69,13 @@
         		  
         		  // Set chart options
         		  var options = {
-        				  chart: {
-        					  title: 'Students By Age Group'
-        				  },
-        				  bars: 'horizontal' // Required for Material Bar Charts.
-        		  };
+        				  'title':'Students By Age Group',
+        				  'width':400,
+        				  'height':300};
 
-        		  var chart = new google.charts.Bar(document.getElementById('barchart_material'));
-
-        		  chart.draw(data, google.charts.Bar.convertOptions(options));
+        		  // Instantiate and draw our chart, passing in some options.
+        		  var chart = new google.visualization.PieChart(document.getElementById('barchart_material'));
+        		  chart.draw(data, options);
     		  }
     	  }
-    	  xmlhttp.send();
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
-
-        // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
       }
