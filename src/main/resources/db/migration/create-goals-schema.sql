c0cc88c70c015b9a660c8302596a16ec3c5d5fe5
Levi Stovall <levi.stovall713@gmail.com>
2020-05-25 23:52:10 -0500

    try adding schema setup with maven

---
+++ src/main/resources/db/migration/create-goals-schema.sql
@@ -0,0 +1,90 @@
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `first_name` varchar(255),
+  `last_name` varchar(255),
+  `user_name` varchar(255),
+  `created_at` timestamp
+);
+
+CREATE TABLE `goals` (
+  `id` int PRIMARY KEY,
+  `owner_id` int,
+  `title` varchar(255),
+  `description` varchar(255),
+  `status` ENUM ('not_started', 'in_progress', 'met'),
+  `goal_type` varchar(255),
+  `created_at` timestamp,
+  `last_updated` timestamp
+);
+
+CREATE TABLE `goal_hierarchy` (
+  `parent_id` int,
+  `child_id` int,
+  `created_at` timestamp,
+  `last_updated` timestamp,
+  PRIMARY KEY (`parent_id`, `child_id`)
+);
+
+CREATE TABLE `goal_types` (
+  `type_name` varchar(255) PRIMARY KEY,
+  `created_at` timestamp,
+  `last_updated` timestamp
+);
+
+CREATE TABLE `goal_hierarchy_rules` (
+  `parent_type_name` varchar(255),
+  `child_type_name` varchar(255),
+  PRIMARY KEY (`parent_type_name`, `child_type_name`)
+);
+
+CREATE TABLE `user_defined_goal_attrs` (
+  `user_id` int,
+  `attr_name` varchar(255),
+  `content_type` ENUM ('plain_text', 'time'),
+  PRIMARY KEY (`attr_name`, `content_type`)
+);
+
+CREATE TABLE `user_defined_goal_attr_types` (
+  `attr_name` varchar(255),
+  `goal_type` varchar(255),
+  PRIMARY KEY (`attr_name`, `goal_type`)
+);
+
+CREATE TABLE `goal_spectators` (
+  `goal_id` int,
+  `user_id` int,
+  PRIMARY KEY (`goal_id`, `user_id`)
+);
+
+CREATE TABLE `goal_collaborators` (
+  `goal_id` int,
+  `user_id` int,
+  PRIMARY KEY (`goal_id`, `user_id`)
+);
+
+ALTER TABLE `goals` ADD FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `goal_types` ADD FOREIGN KEY (`type_name`) REFERENCES `goals` (`goal_type`);
+
+ALTER TABLE `goal_hierarchy` ADD FOREIGN KEY (`parent_id`) REFERENCES `goals` (`id`);
+
+ALTER TABLE `goal_hierarchy` ADD FOREIGN KEY (`child_id`) REFERENCES `goals` (`id`);
+
+ALTER TABLE `goal_hierarchy_rules` ADD FOREIGN KEY (`parent_type_name`) REFERENCES `goal_types` (`type_name`);
+
+ALTER TABLE `goal_hierarchy_rules` ADD FOREIGN KEY (`child_type_name`) REFERENCES `goal_types` (`type_name`);
+
+ALTER TABLE `user_defined_goal_attrs` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `goal_spectators` ADD FOREIGN KEY (`goal_id`) REFERENCES `goals` (`id`);
+
+ALTER TABLE `goal_spectators` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `goal_collaborators` ADD FOREIGN KEY (`goal_id`) REFERENCES `goals` (`id`);
+
+ALTER TABLE `goal_collaborators` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `user_defined_goal_attr_types` ADD FOREIGN KEY (`attr_name`) REFERENCES `user_defined_goal_attrs` (`attr_name`);
+
+ALTER TABLE `user_defined_goal_attr_types` ADD FOREIGN KEY (`goal_type`) REFERENCES `goal_types` (`type_name`);
+
