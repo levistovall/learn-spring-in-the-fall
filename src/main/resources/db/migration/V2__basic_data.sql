b9bc78bd2e5e84785341305f91ee996b45fc9874
Levi Stovall <levi.stovall713@gmail.com>
2020-05-30 22:48:08 -0500

    call sysdate correctly

--- src/main/resources/db/migration/V2__basic_data.sql
+++ src/main/resources/db/migration/V2__basic_data.sql
@@ -1,5 +1,5 @@
-insert into goal_types (type_name, extends, created_at, last_updated) values ('Aspiration', NULL, sysdate, sysdate);
-insert into goal_types (type_name, extends, created_at, last_updated) values ('Travel Plan', NULL, sysdate, sysdate);
-insert into goal_types (type_name, extends, created_at, last_updated) values ('Funding', NULL, sysdate, sysdate);
-insert into goal_types (type_name, extends, created_at, last_updated) values ('Project', NULL, sysdate, sysdate);
-insert into goal_types (type_name, extends, created_at, last_updated) values ('Habit', NULL, sysdate, sysdate);
+insert into goal_types (type_name, created_at, last_updated) values ('Aspiration', sysdate(), sysdate());
+insert into goal_types (type_name, created_at, last_updated) values ('Travel Plan', sysdate(), sysdate());
+insert into goal_types (type_name, created_at, last_updated) values ('Funding', sysdate(), sysdate());
+insert into goal_types (type_name, created_at, last_updated) values ('Project', sysdate(), sysdate());
+insert into goal_types (type_name, created_at, last_updated) values ('Habit', sysdate(), sysdate());
