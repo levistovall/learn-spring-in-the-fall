0e7f3def9366cb16db1321ff53d4f05ad28d3bf8
Levi Stovall <levi.stovall713@gmail.com>
2020-06-12 17:46:15 -0500

    was using request param wrong...

--- src/main/java/LeviController.java
+++ src/main/java/LeviController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.ModelMap;
 
@@ -25,13 +26,13 @@ public class LeviController
 	}
 
 	@RequestMapping(value="/creategoal", method=RequestMethod.POST)
-	public String createGoal(
-			@RequestParam(name = "title") String title,
+	public String createGoal(@RequestBody Goal goal)
+			/*@RequestParam(name = "title") String title,
 			@RequestParam(name = "description") String description,
 			@RequestParam(name = "goalType") String goalType
-			)
+			)*/
 	{
-		template.create(title, description, goalType);
+		template.create(goal.getTitle(), goal.getDescription(), goal.getGoalType());
 		return "created goal";
 	}
 
